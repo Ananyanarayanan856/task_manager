d0fb2088300cd33f14fb11676e55e62300ff5482
Ananya Narayanan <118816924+Ananyanarayanan856@users.noreply.github.com>
2026-05-01 23:26:59 +0530

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
